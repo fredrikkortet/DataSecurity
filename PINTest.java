056aef7bbd6ba9b8524c493a639b881771d445bb
rohkho18 <rohkho18@student.hh.se>
2021-02-23 16:24:37 +0100

    test added

--- PINTest.java
+++ PINTest.java
@@ -6,25 +6,49 @@ import org.junit.jupiter.api.Test;
 
 class PINTest {
 
-
-
-
-		public int initpin = 4321;
-		public PIN testpin;
-		public Field ff ;
-		
-		@BeforeEach 
-		void SetUP() throws NoSuchFieldException, SecurityException {
-			testpin = new PIN(initpin);
-			ff = testpin.getClass().getDeclaredField("access");
-			ff.setAccessible(true);
-		}
+	public int initpin = 4321;
+	public PIN testpin;
+	public Field ff ,counter;
 	
-
+	@BeforeEach
+	void SetUP() throws NoSuchFieldException, SecurityException {
+		testpin = new PIN(initpin);
+		ff = testpin.getClass().getDeclaredField("access");
+		ff.setAccessible(true);
+		counter = testpin.getClass().getDeclaredField("tryCounter");
+		counter.setAccessible(true);
+	}
 	@Test
-	void test1() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+	void test() throws IllegalArgumentException, IllegalAccessException {
+		assertEquals(false,ff.getBoolean(testpin));
+		testpin.checkPin(2222);
+		assertEquals(false,ff.getBoolean(testpin));
+		testpin.checkPin(0000);
+		assertEquals(false,ff.getBoolean(testpin));
+		testpin.checkPin(1111);
+		assertEquals(false, ff.getBoolean(testpin));
+		assertEquals(0,counter.get(testpin));
+		testpin.checkPin(4321);
+		assertEquals(false, ff.getBoolean(testpin));
+		assertEquals(-1,counter.get(testpin));
+	}
+	
+	@Test
+	void test1() throws IllegalArgumentException, IllegalAccessException{
+		testpin.checkPin(0000);
+		assertEquals(false,ff.getBoolean(testpin));
+		testpin.checkPin(1111);
+		assertEquals(false,ff.getBoolean(testpin));
 		testpin.checkPin(4321);
 		assertEquals(true,ff.getBoolean(testpin));
+	}
+	@Test
+	void test2() throws IllegalArgumentException, IllegalAccessException{
+		testpin.checkPin(0);
+		assertEquals(false,ff.getBoolean(testpin));
+		testpin.checkPin(11110);
+		assertEquals(false,ff.getBoolean(testpin));
+		
 	}
 
 }
